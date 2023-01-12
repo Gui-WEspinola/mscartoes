7963fd10e60db8c626a5d35fe493e62d5ba039ac
Guilherme Wanderley Espinola <gwanderleyespinola@gmail.com>
2023-01-12 09:05:56 -0300

    feat: endpoint to find card by income less than or equal

--- src/main/java/io/github/guiwespinola/mscartoes/application/CardsController.java
+++ src/main/java/io/github/guiwespinola/mscartoes/application/CardsController.java
@@ -1,12 +1,14 @@
 package io.github.guiwespinola.mscartoes.application;
 
 import io.github.guiwespinola.mscartoes.application.dto.CardSaveRequest;
+import io.github.guiwespinola.mscartoes.domain.Card;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("cards")
@@ -21,13 +23,19 @@ public class CardsController {
     }
 
     @PostMapping
-    public ResponseEntity<?> saveCard(@RequestBody CardSaveRequest request) {
+    public ResponseEntity<Card> saveCard(@RequestBody CardSaveRequest request) {
         var card = request.toModel();
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .query("name={name}")
                 .buildAndExpand(card.getName())
                 .toUri();
         return ResponseEntity.created(uri).body(cardService.save(card));
+    }
+
+    @GetMapping(params = "income") // /cards?income=5000
+    public ResponseEntity<List<Card>> getCardIncomeLessThan(@RequestParam Long income) {
+        List<Card> cardList = cardService.getCardIncomeLessThanEqual(income);
+        return ResponseEntity.ok(cardList);
     }
 
 }
